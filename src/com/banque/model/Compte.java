7ca56dea5dab35d9e2aa4df1d97b3c9b39b837ff
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-09-17 22:05:14 +0100

    finition code class Compte

--- src/com/banque/model/Compte.java
+++ src/com/banque/model/Compte.java
@@ -9,9 +9,20 @@ public abstract class Compte {
     private double solde ;
     private List<Operation> listOperations = new ArrayList<>();
 
-    public abstract void retirer(double montant);
+    public abstract void retirer(double montant, String destination);
     public abstract double calculerInteret();
     public abstract void afficherDetails();
+
+    public void verser(double montant, String source) {
+        if(montant <= 0) {
+            System.out.println("Montant invalide !");
+            return;
+        }
+        setSolde(getSolde() + montant);
+        Versement v = new Versement(montant, source);
+        getListOperations().add(v);
+        System.out.println("Versement effectué : " + montant);
+    }
 
     public void setCode(String newCode){
         this.code = newCode;
