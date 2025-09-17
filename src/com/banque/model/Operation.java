55937abd589f7b2ccc6a043bdb88f9878dfbd91a
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-09-17 22:07:05 +0100

    finition code class operation

--- src/com/banque/model/Operation.java
+++ src/com/banque/model/Operation.java
@@ -1,4 +1,29 @@
 package com.banque.model;
 
-public class Operation {
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Operation {
+
+    private double montant;
+    private UUID numero;
+    private LocalDate date;
+
+    public Operation(double montant){
+        this.montant = montant;
+        this.numero = UUID.randomUUID();
+        this.date = LocalDate.now();
+    }
+    public void setMontant(double NewMontant){
+        this.montant = NewMontant;
+    }
+    public double getMontant(){
+        return montant;
+    }
+    public UUID getNumero(){
+        return  numero;
+    }
+    public LocalDate getDate(){
+        return date;
+    }
 }
