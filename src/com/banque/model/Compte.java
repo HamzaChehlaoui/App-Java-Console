5a9d6d04e45f33fdb0f55658ea557c2062c8397c
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-09-17 15:20:04 +0100

    ajouter code class Compte et CompteCourant creat les methods abstract et les attrubutes

--- src/com/banque/model/Compte.java
+++ src/com/banque/model/Compte.java
@@ -1,4 +1,30 @@
 package com.banque.model;
 
-public class Compte {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Compte {
+
+    private String code ;
+    private double solde ;
+    private List<Operation> listOperations = new ArrayList<>();
+
+    public abstract void retirer(double montant);
+    public abstract double calculerInteret();
+    public abstract void afficherDetails();
+
+    public void setCode(String newCode){
+        this.code = newCode;
+
+    }
+    public String getCode(){
+        return code;
+    }
+    public void setSolde(double newSolde){
+        this.solde = newSolde;
+    }
+    public double getSolde(){
+        return solde;
+    }
+    public List<Operation> getListOperations(){return listOperations;}
 }
--- src/com/banque/model/CompteCourant.java
+++ src/com/banque/model/CompteCourant.java
@@ -1,4 +1,30 @@
 package com.banque.model;
 
-public class CompteCourant {
+public class CompteCourant extends Compte {
+
+    private double decouvert ;
+
+    @Override
+    public void retirer(double montant){
+
+    }
+
+    @Override
+    public  void afficherDetails(){
+        System.out.println("Le solde du compte courant est : " + getSolde());
+        System.out.println("Solde negatif minimum autorisé : " + decouvert);
+
+    }
+
+    @Override
+    public double calculerInteret(){
+        return 0;
+    }
+
+    public double getDecouvert(){
+        return decouvert;
+    }
+    public void setDecouvert(double Newdecouvert){
+        this.decouvert = Newdecouvert;
+    }
 }
